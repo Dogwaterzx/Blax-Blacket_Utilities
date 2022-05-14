782be580635279326ea735cd23835ea45f07c20b
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-05-13 20:10:16 -0400

    Create blaxError.js
    Edited version

---
+++ resources/blaxError.js
@@ -0,0 +1,6 @@
+function error() {
+    if (confirm("an error has ocured, would you like to report this on github?")) {
+        window.open("https://github.com/ZasticBradyn/blacket-hacks/issues" or "htps://github.com/Dogwaterzx/Blax-Blacket_Utilities/issues))
+    }
+    console.error("an error has occured")
+}
